a0a4fa0e8fb2c19b601473d10cc9a836b28e24e6
Mo Di <ddddmo@outlook.com>
2025-12-14 14:06:49 +0800

    升级默认版本至 18.6.2；优化 stop 脚本，解决超时问题；添加 backup 脚本

---
+++ backup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker compose exec gitlab gitlab-backup create
--- stop.sh
+++ stop.sh
@@ -4,5 +4,7 @@
 
 set -eux
 
+# 先停 sidekiq，sidekiq 在其他服务之后退出会超时
+docker compose exec gitlab gitlab-ctl stop sidekiq
 docker compose exec gitlab gitlab-ctl stop
 docker compose stop gitlab
